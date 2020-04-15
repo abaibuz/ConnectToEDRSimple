4e8287f29f9af2e29c72b6e36fc8b07404d5b169
obaibuz <abaybuz@ukr.net>
2020-04-15 16:45:25 +0300

    create 3

---
+++ SCRViewControl/SCRViewControllerObjC.h
@@ -0,0 +1,13 @@
+//
+//  SCRViewControllerObjC.h
+//  UIScrollViewWithAutoLayout
+//
+//  Created by Mike Woelmer on 2/17/14.
+//  Copyright (c) 2014 atomicobject. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCRViewControllerObjC : UIViewController <UITextFieldDelegate>
+
+@end
